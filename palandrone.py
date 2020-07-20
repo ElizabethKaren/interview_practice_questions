07b807093cae6019ed336b01c28ea6beada9d858
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-20 19:01:15 -0400

    is_substring

--- palandrone.py
+++ palandrone.py
@@ -1,4 +1,4 @@
-string1 = 'elizabeth'
+string1 = 'elizabkienji'
 string2 = 'liz'
 
 def isPalindrome(string):
@@ -16,6 +16,14 @@ def isPalindrome(string):
     return False
 
 def isSubstring(string1, string2):
+    sub_string = ''
+    for letter in string2:
+        for letter2 in string1:
+            if letter2 == letter:
+                sub_string += letter2
+
+    return len(sub_string) >= len(string2)
+
 
 
 print(isSubstring(string1,string2))
