8a19ffc1da0d37ce502843647acd0d0d1549b062
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-20 12:29:21 -0400

    palindrome_permutation

---
+++ palindrome_permutation.py
@@ -0,0 +1,26 @@
+#given a string write a function to check if it is a permutation of a palindrome
+
+
+input = 'Tact Coa' ##true
+input2 = 'eloxi'
+
+def is_palindrome_permutation(input):
+    dictionary = {}
+    for i in range(len(input)):
+        key = input[i]
+        if key == ' ':
+            pass
+        elif key in dictionary.keys():
+            dictionary[key] += 1 
+        else:
+            dictionary[key] = 1
+
+    for key in dictionary:
+        if dictionary[key] % 2 == 0:
+            return True
+    
+    return False
+
+
+
+print(is_palindrome_permutation(input2))
