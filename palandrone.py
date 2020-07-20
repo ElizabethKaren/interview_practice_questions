db8dbf7094cfd99f7d0c7156234393b716773b4f
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-20 18:33:35 -0400

    more

--- palandrone.py
+++ palandrone.py
@@ -1,10 +1,21 @@
-string = 'grapefruit'
+string1 = 'elizabeth'
+string2 = 'liz'
 
 def isPalindrome(string):
-    newString = ''
-    for i in reversed(range(len(string))):
-        newString += string[i]
+    d = {}
+    for letter in string:
+        if letter in d.keys():
+            d[letter] += 1
+        else:
+            d[letter] = 1
+    
+    for key in d:
+        if d[key] % 2 == 0:
+            return True
+    
+    return False
 
-    return newString
+def isSubstring(string1, string2):
 
-print(isPalindrome(string))
+
+print(isSubstring(string1,string2))
