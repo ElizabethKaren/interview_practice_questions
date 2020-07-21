e12d789feeb0248ea4aa1c639e98677e09cffd3e
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-21 11:35:14 -0400

    max_non_adddddddjacent

--- max_sum.py
+++ max_sum.py
@@ -1,22 +1,22 @@
 array = [7,10,12,7,9,14]
+array2 = [9]
 
 def max_subset_sum_no_adjacent(array):
-    d = {}
-    higest_num = 0
-    for i in range(len(array)-1):
-        num = find_highest_num(array[i], array[i-2], array[i-4])
-        d[num] = [array[i], array[i-2], array[i-4]]
+    if not len(array):
+        return 0
+    elif len(array) == 1:
+        return array[0]
+    max_sums = array[:]
+    max_sums[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+        max_sums[i] = max(max_sums[i-1], max_sums[i-2] + array[i])
+    return max_sums[-1]
+  
 
-    for key in d:
-        if key > higest_num:
-            higest_num = key
 
-    return higest_num
-
-
-def find_highest_num(num1, num2=0, num3=0):
-    sum = num1 + num2 + num3 
-    return sum
+# def find_highest_num(num1, num2=0, num3=0):
+#     sum = num1 + num2 + num3 
+#     return sum
 
 
 print(max_subset_sum_no_adjacent(array))
