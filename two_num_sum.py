640e251cc41197fe08223df19251a42a221d589c
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-22 11:25:33 -0400

    two_nums_w_binary_sort

--- two_num_sum.py
+++ two_num_sum.py
@@ -1,7 +1,7 @@
 array = [3,5-4,8,11,1,-1,6]
 target_sum = 10
 
-def two_nums(array,target_sum):
+def two_nums_n_squared(array,target_sum):
     dictionary = {}
     for num in array:
         y = target_sum - num
@@ -11,6 +11,20 @@ def two_nums(array,target_sum):
     
     return []
 
+
+def two_nums(array,target_sum):
+    array.sort()
+    left = 0
+    right = len(array)-1
+    while right > left:
+        currentSum = array[right] + array[left]
+        if currentSum == target_sum:
+            return [array[right], array[left]]
+        elif currentSum < target_sum:
+            left += 1 
+        elif currentSum > target_sum:
+            right -= 1
+    return []
 
 
 
