f98b10b293b36fa633bdec4b597407ccdb3fff46
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-24 12:21:24 -0400

    island

---
+++ permiter.py
@@ -0,0 +1,35 @@
+Input = [
+    [0,1,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [1,1,0,0]
+ ]
+
+#Output: 16
+
+def islandPerimeter(grid):
+        perimeter = 0
+        row = 0
+        while row < len(grid):
+            inner_array = grid[row]
+            col = 0
+            while col < len(inner_array):
+                print(row, col)
+                if grid[row][col] == 1:
+                    if grid[row][col -1] == 0:
+                        perimeter += 1
+                    if grid[row][col + 1] == 0:
+                        perimeter += 1
+                    if grid[row + 1][col + 1] == 0:
+                        perimeter += 1
+                    if grid[row - 1][col + 1] == 0:
+                        perimeter += 1
+                col += 1
+            row += 1
+
+        return perimeter
+        
+        #grid[0] = row
+        #grid[0][0] = row, col, specific value
+
+print(islandPerimeter(Input))
