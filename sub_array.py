80d84a06764e1667684863e71591a824f235c719
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-08-01 12:18:42 -0400

    completedSubArraySum

--- sub_array.py
+++ sub_array.py
@@ -10,17 +10,22 @@ y = 2
 def max_sum_sub_array(array,k):
     max_sum = 0 
     sub_array = []
-    for num in array:
-        new_array = count_sub_array(array[num:], k)
-        if new_array.sum() > max_sum:
+    for index in range(len(array)):
+        new_array = count_sub_array(array[index:], k)
+        if sum(new_array) > max_sum:
             sub_array = new_array
-            max_sum = new_array.sum()
+            max_sum = sum(new_array)
 
     return sub_array
 
 def count_sub_array(array, k):
-    print(array)
+    if len(array) < k:
+        return array
+    return_array = []
+    for num in range(k):
+        return_array.append(array[num])
+
+    return return_array
 
 
-
-print(max_sum_sub_array(array,k))
+print(max_sum_sub_array(array2,y))
