6e9b4159462ba1df529d5acf3f53303053d2decd
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-08-03 14:56:17 -0400

    singleNuM

--- single_num.py
+++ single_num.py
@@ -1,18 +1,34 @@
-# array = [2,2,1]
-# Output: 1
-# Example 2:
+# # array = [2,2,1]
+# # Output: 1
+# # Example 2:
 
-array = [4,1,2,1,2]
-# Output: 4
+# array = [4,1,2,1,2]
+# # Output: 4
+
+# def single_num(array):
+#     for index in range(len(array)):
+#         num = array[index]
+#         arr = array[:index] + array[index+1:]
+#         if num not in arr:
+#             return num
+
+# array = [2,2,3,2]
+# Output: 3
+
+array = [0,1,0,1,0,1,99]
+# Output: 99
 
 def single_num(array):
-    for index in range(len(array)):
-        num = array[index]
-        arr = array[:index] + array[index+1:]
-        if num in arr:
-            pass 
+    d = {}
+    for num in array:
+        if num in d:
+            d[num] += 1
         else:
-            return num
+            d[num] = 1
+    
+    for key in d:
+        if d[key] == 1:
+            return key
 
 
 
