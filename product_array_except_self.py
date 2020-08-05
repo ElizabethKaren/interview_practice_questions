71ee7976b7abf3050373e2818a016b32a171c124
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-08-05 19:56:58 -0400

    notthereyet

--- product_array_except_self.py
+++ product_array_except_self.py
@@ -5,16 +5,34 @@
 array = [1,2,3,4]
 # Output: [24,12,8,6]
 
+# def product_except_self(array):
+#     total = 1
+#     for num in array:
+#         total = total * num
+
+#     for index in range(len(array)):
+#         new_num = total/array[index]
+#         array[index] = int(new_num)
+
+#     return array
+
 def product_except_self(array):
+    return_array = []
+    for index in range(len(array)):
+        new_arr = array[:index] + array[index+1:]
+        num = helper(new_arr)
+        return_array.append(num)
+
+    return return_array
+
+
+def helper(new_arr):
+    print(array)
     total = 1
-    for num in array:
+    for num in new_arr:
         total = total * num
 
-    for index in range(len(array)):
-        new_num = total/array[index]
-        array[index] = int(new_num)
-
-    return array
+    return total
 
 
 print(product_except_self(array))
