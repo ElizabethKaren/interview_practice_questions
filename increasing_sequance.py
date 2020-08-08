d66aabe78a7bf91239b4332abee7f260d522b61b
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-08 11:30:02 -0400

    almostincreasing

---
+++ increasing_sequance.py
@@ -0,0 +1,13 @@
+sequence = [1, 3, 2, 1] #false
+
+
+def almostIncreasingSequence(sequence):
+    c = 0
+    for i in range(len(sequence)-1):
+        if sequence[i] >= sequence[i+1]: c += 1
+        if i+2 < len(sequence) and sequence[i] >= sequence[i+2]: c += 1
+    return c < 3
+
+
+
+print(almostIncreasingSequence(sequence))
