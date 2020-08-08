df0816a10d349b5863ee1d6df7cd0003bc0c5481
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-08 10:00:41 -0400

    DRYcenturyfromyear

--- new_one.py
+++ new_one.py
@@ -1,7 +1,7 @@
-# year = 1905
+year = 1905
 # the output should be
 # centuryFromYear(year) = 20;
-year = 1700
+# year = 1700
 #the output should be
 # centuryFromYear(year) = 17.
 # year = 200
@@ -9,26 +9,8 @@ year = 1700
 
 
 def centuryFromYear(year):
-    time = str(year)
-    if len(time) == 4:
-        end_of_time = time[2:]
-    else:
-        end_of_time = time[1:]
-
-    for num in end_of_time:
-        if int(num) != 0:
-           return how_many_digets(str(year), 1)
-
-    return how_many_digets(str(year))
+    return (year -1 )//100 + 1
     
     
-
-def how_many_digets(year, num=0):
-    if len(year) == 4:
-        return int(year[0] + year[1]) + num
-    elif len(year) == 3:
-        return int(year[0]) + num
-    else:
-        return 1
 
 print(centuryFromYear(year))
