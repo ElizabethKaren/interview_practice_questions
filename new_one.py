e821518677aeae1315cd9e5502e063d759fa0888
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-07 23:41:45 -0400

    centuryFromYear

--- new_one.py
+++ new_one.py
@@ -1,18 +1,30 @@
-# year = 1905
+year = 1905
 # the output should be
 # centuryFromYear(year) = 20;
-year = 1700
+#year = 1700
 #the output should be
 # centuryFromYear(year) = 17.
+# year = 200
+# output = 2
 
 
 def centuryFromYear(year):
     time = str(year)
-    print(time[3])
-    if int(time[2]) == 0 and int(time[3]) == 0:
-        return time[0] + time[1]
-    else:
-        return int(time[0] + time[1]) + 1
+    end_of_time = time[1:]
+    for num in end_of_time:
+        if num != 0:
+           return how_many_digets(str(year), 1)
 
+    return how_many_digets(year)
+    
+    
+
+def how_many_digets(year, num=0):
+    if len(year) == 4:
+        return int(year[0] + year[1]) + num
+    elif len(year) == 3:
+        return int(year[0]) + num
+    else:
+        return 1
 
 print(centuryFromYear(year))
