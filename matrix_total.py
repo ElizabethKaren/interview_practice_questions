f6cdd984142203499eb3d2bacf654494dd7a0f2f
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-08 12:58:56 -0400

    matrixtotal

--- matrix_total.py
+++ matrix_total.py
@@ -2,21 +2,16 @@ matrix= [[1,1,1,0],
         [0,5,0,1], 
         [2,1,3,10]]
 
-def matrixElementsSum(matrix):
-    total_sum = 0
-    outter_count = 0
-    while outter_count < len(matrix):
-        inner_count = 0
-        while inner_count < len(matrix[outter_count]):
-            new_num = matrix[outter_count][inner_count]
-            if outter_count + 1 < len(matrix): 
-                possible_zero = matrix[outter_count+1][inner_count]
-                if possible_zero != 0:
-                    print(new_num)
-                    total_sum += new_num
-            inner_count += 1
-        outter_count +=1
-    
-    return total_sum
+def matrixElementsSum(m):
+    r = len(m)
+    c = len(m[0])
+    total=0
+    for j in range(c):
+        for i in range(r):
+            if m[i][j]!=0:
+                total+=m[i][j]
+            else:
+                break
+    return total
 
 print(matrixElementsSum(matrix))
