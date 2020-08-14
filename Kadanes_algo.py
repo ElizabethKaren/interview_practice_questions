27018f80cb0513e9a79925fdc1f06a3e4a1f430a
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-14 12:52:27 -0400

    kanades

---
+++ Kadanes_algo.py
@@ -0,0 +1,16 @@
+#write a function that takes a non empty array of integers
+#and returns the maximum sum that can be obtained by 
+#summing up all of the integers in a non empty
+#subarray fo the input array. a subarray must
+#only contain adgacent numbers 
+
+
+array = [3,5,-9,1,3,-2,3,4,7,2,-9,6,3,1,-5,4]
+#19
+
+
+def kadanes_algo(array):
+    return array
+
+
+print(kadanes_algo(array))
