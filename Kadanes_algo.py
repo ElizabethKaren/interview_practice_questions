878ac65e062b5a1f5ce63f2fb27ea515a801358e
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-14 13:11:35 -0400

    kananes

--- Kadanes_algo.py
+++ Kadanes_algo.py
@@ -10,7 +10,13 @@ array = [3,5,-9,1,3,-2,3,4,7,2,-9,6,3,1,-5,4]
 
 
 def kadanes_algo(array):
-    return array
+    max_ending_here = array[0]
+    max_so_far = array[0]
+    for i in range(1,len(array)):
+        num = array[i]
+        max_ending_here = max(num, max_ending_here + num)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
 
 
 print(kadanes_algo(array))
