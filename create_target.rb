f8ac41f447a5978205d37c4e879358cd70d90aec
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-18 15:22:34 -0400

    ruby

---
+++ create_target.rb
@@ -0,0 +1,29 @@
+#Given two arrays of integers nums and index. 
+#Your task is to create target array under the 
+#following rules:
+#Initially target array is empty.
+#From left to right read nums[i] and index[i], 
+#insert at index index[i] the value nums[i] in target array.
+#Repeat the previous step until there are no elements to read 
+#in nums and index. Return the target array.
+#It is guaranteed that the insertion operations will be valid.
+
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+#Output: [0,4,1,3,2]
+
+def create_target(nums,index)
+    return_arr = []
+    nums.each do |i|
+        new_index = index[i]
+        if new_index < return_arr.size
+            return_arr[new_index] = nums[i]
+        else
+            return_arr << nums[i]
+        end
+    end 
+    p return_arr
+end 
+
+
+create_target(nums,index)
