fd4eb0bc8e06be28315d11e0b2add0b0d4185926
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-18 09:04:28 -0400

    nope

--- split_a_string.py
+++ split_a_string.py
@@ -1,4 +1,4 @@
-#balanced st4rings are those who have qual quantity of L and R characters
+#balanced strings are those who have qual quantity of L and R characters
 #given a balanced string s split it in the maximum amount of balanced strings
 #return the max amount of splitted balanced strings 
 
@@ -7,3 +7,18 @@ s = 'RLRRLLRLRL'
 
 #Explanation: s can be split into "RL", "RRLL", "RL", "RL", 
 #each substring contains same number of 'L' and 'R'.
+
+def balanced(string):
+    count = 0
+    for index in range(len(string)-1):
+        i = string[index]
+        y = string[index + 1]
+        if i != y:
+            pass 
+        else:
+            count += 1
+    
+    return count 
+
+
+print(balanced(s))
