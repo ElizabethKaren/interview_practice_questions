9182ab18830c3e548cb0e9f264bddd0d471c3529
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-08-19 09:52:14 -0400

    home_robber_algo

--- create_target.rb
+++ create_target.rb
@@ -8,13 +8,24 @@
 #in nums and index. Return the target array.
 #It is guaranteed that the insertion operations will be valid.
 
-nums = [0,1,2,3,4]
-index = [0,1,2,2,1]
+# nums = [0,1,2,3,4]
+# index = [0,1,2,2,1]
 #Output: [0,4,1,3,2]
+
+nums = [1,2,3,4,0]
+index = [0,1,2,3,0]
+#output: [0,1,2,3,4]
 
 def create_target(nums,index)
     return_arr = []
-    nums.each { |i|return_arr.insert(index[i], nums[i]) } 
+    nums.each do |i|
+        if return_arr.size < index[i]
+            return_arr << nums[i]
+        else
+            return_arr.insert(index[i], nums[i])
+        end
+        p return_arr
+    end
     p return_arr
 end 
 
