e99832d5b69e15fca4a3ed19e5bad5aae7199afa
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-12 16:32:00 -0400

    fatArrow

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,20 +1,15 @@
-
-
-
 array = [8,5,2,9,5,6,3]
 
 bubbleSort = array => {
     let isSorted = false;
-    let counter = 0;
     while (!isSorted) {
         isSorted = true;
-        for (let i = 0; i < array.length - 1 - counter; i++){
+        for (let i = 0; i < array.length - 1; i++){
             if (array[i] > array[i+1]){
                 swap(i, i+1, array);
                 isSorted = false
             }
         }
-        counter ++;
     }
     return array;
 }
@@ -27,3 +22,6 @@ swap = (i,j,array) => {
 
 
 console.log(bubbleSort(array))
+
+
+
