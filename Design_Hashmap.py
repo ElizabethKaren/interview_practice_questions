ef69ddedf75f453d5fa5e61e3406e5d116216ffe
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-10-30 16:40:24 -0400

    hashMap

---
+++ Design_Hashmap.py
@@ -0,0 +1,27 @@
+#Design a HashMap without using any built-in hash table libraries.
+
+#To be specific, your design should include these functions:
+
+#put(key, value) : Insert a (key, value) pair into the HashMap. 
+# If the value already exists in the HashMap, update the value.
+#get(key): Returns the value to which the specified key is mapped, 
+# or -1 if this map contains no mapping for the key.
+#remove(key) : Remove the mapping for the value key if this map 
+# contains the mapping for the key.
+
+def put(num1 ,num2):
+    return num1 + num2 
+
+
+def get(num):
+    return num
+
+
+def remove(num):
+    return num
+
+
+print(put(1,2))
+print(put(2,2))
+print(get(1))
+print(remove(2))
