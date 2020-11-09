2aa55dcd5c3824355c704a218a176770356d5f8e
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-09 15:43:51 -0500

    max_profit

---
+++ Question.py
@@ -0,0 +1,7 @@
+tickets = [4,3,4,8,4,8]
+
+def mixTicks(tickets):
+    return tickets.sort()
+
+
+print(mixTicks(tickets))
---
+++ max_profit.py
@@ -0,0 +1,9 @@
+#You're given an array of positive integers representing the prices of a single stock on various days 
+# (each index in the array represents a different day). You're also given an integer K, which represents 
+# the number of transactions you're allowed to make. One transaction consists of buying the stock on a 
+# given day and selling it on another, later day. You're asked to write a function that returns the 
+# maximum profit that you can make by buying and selling the stock given K transactions. 
+#Note you can only hold one share of the stock at a time; in other words, you cant buy more than one 
+# share of the stock on any given day, and you cant buy more than one share of the stock on any given 
+# day, and you cant buy a share of the stock if you're still holding another share. Also, you don't 
+# need to use all K transactions that you're allowed.
