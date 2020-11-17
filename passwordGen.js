b700902fb4db414ff68e722cca059be3030461b7
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-16 21:39:54 -0500

    woah

--- passwordGen.js
+++ passwordGen.js
@@ -1,7 +1,7 @@
-const passwordGenerator = () => {
+const passwordGenerator = (passwordNeededLength) => {
     const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
     let string = ''
-    let length = Math.floor(Math.random() * 12)
+    let length = passwordNeededLength
     let count = 0 
     while (count < length){
         let randomChar = Math.floor(Math.random() * characters.length)
@@ -11,7 +11,7 @@ const passwordGenerator = () => {
     return string
 }
 
-console.log(passwordGenerator())
+console.log(passwordGenerator(12))
 
 
 
