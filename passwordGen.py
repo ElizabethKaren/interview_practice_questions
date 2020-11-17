560d4cc0d682cd05213299bc285d6ab57ef11e3c
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-16 21:06:41 -0500

    shorter

--- passwordGen.py
+++ passwordGen.py
@@ -6,12 +6,11 @@ import random
 def passwordGenerator():
     string = ''
     characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
-    count = 0
     length = random.randint(8,12)
-    while count < length:
+    for count in range(length):
         randomChar = random.randint(0,len(characters)-1)
         string += characters[randomChar]
-        count += 1 
+
     return string 
 
 
