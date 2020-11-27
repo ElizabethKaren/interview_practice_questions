74fa5e426c0ca85cfda6d56e026f386e2007a744
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-27 11:34:48 -0500

    update

--- passwordGen.py
+++ passwordGen.py
@@ -3,16 +3,15 @@
 # importing the random module
 import random
 
-def passwordGenerator():
+def password_generator(length=12):
     string = ''
     characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
-    length = random.randint(8,12)
     for count in range(length):
-        randomChar = random.randint(0,len(characters)-1)
-        string += characters[randomChar]
+        random_char = random.randint(0,len(characters)-1)
+        string += characters[random_char]
 
     return string 
 
 
-print(passwordGenerator())
+print(password_generator())
 
