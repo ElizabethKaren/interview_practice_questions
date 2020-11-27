7448491a486b5fef2eea9e620ee54c9510f84b26
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-27 12:00:31 -0500

    update

--- passwordGen.py
+++ passwordGen.py
@@ -20,4 +20,12 @@ import random
 def password_generator(length=12):
     string = ''
     characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
+    for i in range(length):
+        random_char = random.randint(0,len(characters)-1)
+        string += characters[random_char]
     return string 
+
+
+
+
+print(password_generator())
