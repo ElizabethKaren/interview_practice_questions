04d837e157ff4d9581483ea471487ae59f9e1fde
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-11-27 11:43:34 -0500

    update

--- passwordGen.js
+++ passwordGen.js
@@ -1,17 +1,26 @@
-const passwordGenerator = (passwordNeededLength=12) => {
-    const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
+// const passwordGenerator = (passwordNeededLength=12) => {
+//     const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
+//     let string = ''
+//     let length = passwordNeededLength
+//     let count = 0 
+//     while (count < length){
+//         let randomChar = Math.floor(Math.random() * characters.length)
+//         string = string + characters[randomChar]
+//         count ++ 
+//     }
+//     return string
+// }
+
+// console.log(passwordGenerator())
+
+const passwordGenerator = (length=12) => {
     let string = ''
-    let length = passwordNeededLength
-    let count = 0 
-    while (count < length){
-        let randomChar = Math.floor(Math.random() * characters.length)
-        string = string + characters[randomChar]
-        count ++ 
-    }
+    const characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
     return string
 }
 
-console.log(passwordGenerator())
+
+
 
 
 
--- passwordGen.py
+++ passwordGen.py
@@ -3,15 +3,21 @@
 # importing the random module
 import random
 
+# def password_generator(length=12):
+#     string = ''
+#     characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
+#     for count in range(length):
+#         random_char = random.randint(0,len(characters)-1)
+#         string += characters[random_char]
+
+#     return string 
+
+
+# print(password_generator())
+
+
+
 def password_generator(length=12):
     string = ''
     characters = ['a','b','c','d','e','f','g','!' ,'@','$','%','k','l', 'm', 'n','o','p','2','4','6','7','8','9', 'Q', 'R', 'S','T','U','V','W','X','Y', 'Z']
-    for count in range(length):
-        random_char = random.randint(0,len(characters)-1)
-        string += characters[random_char]
-
     return string 
-
-
-print(password_generator())
-
